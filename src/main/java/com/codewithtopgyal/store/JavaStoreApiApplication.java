5e7d938a009534bd2a6a955d680b69e37c5c793a
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-07-28 20:42:27 -0400

    configure mysql-db add mvc starter, thymeleaf template engine, dynamic routes, dt s l;

---
+++ src/main/java/com/codewithtopgyal/store/JavaStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.codewithtopgyal.javastoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithtopgyal/store/JavaStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithtopgyal.javastoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
